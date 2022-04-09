30be35216f810b212c9921a1866548bf7bdcf64e
Angga ps <80311292+anggaps@users.noreply.github.com>
2022-04-10 02:25:19 +0700

    data validation implemented

--- src/handler.js
+++ src/handler.js
@@ -9,7 +9,12 @@ const addNoteHandler = (request, h) => {
   const updatedAt = createdAt;
 
   const newNote = {
-    title, tags, body, id, createdAt, updatedAt,
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
   };
 
   notes.push(newNote);
